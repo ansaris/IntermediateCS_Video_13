f1a6f35c7d83d7fb1949afa509dfc3e0e972705b
ansaris <salman.ansari.usa@gmail.com>
2016-05-24 15:15:00 -0500

    Add instance fields and static field to class.

--- IntermediateCS_Video_13/Program.cs
+++ IntermediateCS_Video_13/Program.cs
@@ -1,15 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace IntermediateCS_Video_13
+class FieldExample
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        }
-    }
+    // A field declaration is like a local variable declaration...
+
+    string name = "Eric";
+
+    // but the "var" syntax, unfortunately, is not supported for fields.
+    // var birthday = new DateTime(1972, 11, 30);
+
+    readonly decimal dollars;
+
+    // A field initializer may not access "this" because experience has show that
+    // practice to be a common cause of bugs.
+
+    // int length = this.name.Length;
+
+    // Fields are by default "instance" fields. A "static" field is a variable
+    // associated with the class, not an instance of the class.
+    static int count = 0;
+
 }
